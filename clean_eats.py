4c709fbd58d6420c6859b20ced4a69cc2c6249ef
LukeCreativeInd <71154774+LukeCreativeInd@users.noreply.github.com>
2025-08-18 13:02:12 +1000

    Update clean_eats.py

--- clean_eats.py
+++ clean_eats.py
@@ -92,6 +92,7 @@ def run():
                 "State": state,
                 "Country": country,
                 "Deliver to": order["Shipping Name"],
+                "Company": order.get("Shipping Company", ""),
                 "Phone No.": phone,
                 "Time Window": "0600-1800",
                 "Group": "Clean Eats Australia",

e0369f6bcfa66b59e385ed31cd6a597db1c52cd2
LukeCreativeInd <71154774+LukeCreativeInd@users.noreply.github.com>
2025-08-18 13:05:26 +1000

    Update made_active.py

--- made_active.py
+++ made_active.py
@@ -80,6 +80,7 @@ def run():
             "State": state,
             "Country": country,
             "Deliver to": order["Shipping Name"],
+                "Company": order.get("Shipping Company", ""),
             "Phone No.": phone,
             "Time Window": "0600-1800",
             "Group": "Made Active",
